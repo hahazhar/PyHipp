48628ccab88054c3736a12d117c0fb8f381eb364
AWS ParallelCluster user <ec2-user@ip-10-0-14-42.ap-southeast-1.compute.internal>
2020-10-29 13:00:45 +0000

    adding the new scripts from lab 8

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -12,7 +12,8 @@
 #SBATCH -e fsall-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-python -u -c "import PyHipp as pyh; import time;
+python -u -c "import PyHipp as pyh;import DataProcessingTools as DPT;
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1);
+lfall.save();
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
 hfall.save();"
